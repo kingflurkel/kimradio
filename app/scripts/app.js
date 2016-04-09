168440a702e57baf93a2ce180b204df3b59f28c3
kingflurkel <mikethuy@gmail.com>
2016-04-10 01:35:22 +0200

    ..

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -52,4 +52,9 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     document.getElementById('mainContainer').scrollTop = 0;
   };
 
+  app.changebg = function(){
+    console.log('rec end');
+
+  };
+
 })(document);
